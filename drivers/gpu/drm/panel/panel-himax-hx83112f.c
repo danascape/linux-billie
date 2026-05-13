9e64a1b5c7fe2744f0a536444ac6810550bc340d
Saalim Quadri <danascape@gmail.com>
2026-05-14 02:44:07 +0530

    test
    Signed-off-by: Saalim Quadri <danascape@gmail.com>

--- drivers/gpu/drm/panel/panel-himax-hx83112f.c
+++ drivers/gpu/drm/panel/panel-himax-hx83112f.c
@@ -24,7 +24,14 @@ struct hx83112f_panel {
 	struct drm_panel panel;
 	struct mipi_dsi_device *dsi;
 	struct drm_dsc_config dsc;
+	struct regulator_bulk_data *supplies;
 	struct gpio_desc *reset_gpio;
+};
+
+static const struct regulator_bulk_data hx83112f_supplies[] = {
+	{ .supply = "vddio" },
+	{ .supply = "vsp" },
+	{ .supply = "vsn" },
 };
 
 static inline struct hx83112f_panel *to_hx83112f_panel(struct drm_panel *panel)
@@ -92,12 +99,19 @@ static int hx83112f_prepare(struct drm_panel *panel)
 	struct drm_dsc_picture_parameter_set pps;
 	int ret;
 
+	ret = regulator_bulk_enable(ARRAY_SIZE(hx83112f_supplies), ctx->supplies);
+	if (ret < 0) {
+		dev_err(dev, "Failed to enable regulators: %d\n", ret);
+		return ret;
+	}
+
 	hx83112f_reset(ctx);
 
 	ret = hx83112f_on(ctx);
 	if (ret < 0) {
 		dev_err(dev, "Failed to initialize panel: %d\n", ret);
 		gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+		regulator_bulk_disable(ARRAY_SIZE(hx83112f_supplies), ctx->supplies);
 		return ret;
 	}
 
@@ -131,6 +145,7 @@ static int hx83112f_unprepare(struct drm_panel *panel)
 		dev_err(dev, "Failed to un-initialize panel: %d\n", ret);
 
 	gpiod_set_value_cansleep(ctx->reset_gpio, 1);
+	regulator_bulk_disable(ARRAY_SIZE(hx83112f_supplies), ctx->supplies);
 
 	return 0;
 }
@@ -145,8 +160,8 @@ static const struct drm_display_mode hx83112f_mode = {
 	.vsync_start = 2400 + 1300,
 	.vsync_end = 2400 + 1300 + 15,
 	.vtotal = 2400 + 1300 + 15 + 15,
-	.width_mm = 0,
-	.height_mm = 0,
+	.width_mm = 68,
+	.height_mm = 150,
 	.type = DRM_MODE_TYPE_DRIVER,
 };
 
@@ -162,6 +177,41 @@ static const struct drm_panel_funcs hx83112f_panel_funcs = {
 	.get_modes = hx83112f_get_modes,
 };
 
+static int hx83112f_bl_update_status(struct backlight_device *bl)
+{
+	struct mipi_dsi_device *dsi = bl_get_data(bl);
+	u16 brightness = backlight_get_brightness(bl);
+	int ret;
+
+	dsi->mode_flags &= ~MIPI_DSI_MODE_LPM;
+
+	ret = mipi_dsi_dcs_set_display_brightness_large(dsi, brightness);
+	if (ret < 0)
+		return ret;
+
+	dsi->mode_flags |= MIPI_DSI_MODE_LPM;
+
+	return 0;
+}
+
+static const struct backlight_ops hx83112f_bl_ops = {
+	.update_status = hx83112f_bl_update_status,
+};
+
+static struct backlight_device *
+hx83112f_create_backlight(struct mipi_dsi_device *dsi)
+{
+	struct device *dev = &dsi->dev;
+	const struct backlight_properties props = {
+		.type = BACKLIGHT_RAW,
+		.brightness = 4095,
+		.max_brightness = 4095,
+	};
+
+	return devm_backlight_device_register(dev, dev_name(dev), dev, dsi,
+					      &hx83112f_bl_ops, &props);
+}
+
 static int hx83112f_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
@@ -173,6 +223,13 @@ static int hx83112f_probe(struct mipi_dsi_device *dsi)
 				   DRM_MODE_CONNECTOR_DSI);
 	if (IS_ERR(ctx))
 		return PTR_ERR(ctx);
+
+	ret = devm_regulator_bulk_get_const(dev,
+					    ARRAY_SIZE(hx83112f_supplies),
+					    hx83112f_supplies,
+					    &ctx->supplies);
+	if (ret < 0)
+		return ret;
 
 	ctx->reset_gpio = devm_gpiod_get(dev, "reset", GPIOD_OUT_HIGH);
 	if (IS_ERR(ctx->reset_gpio))
@@ -188,6 +245,11 @@ static int hx83112f_probe(struct mipi_dsi_device *dsi)
 			  MIPI_DSI_CLOCK_NON_CONTINUOUS | MIPI_DSI_MODE_LPM;
 
 	ctx->panel.prepare_prev_first = true;
+
+	ctx->panel.backlight = hx83112f_create_backlight(dsi);
+	if (IS_ERR(ctx->panel.backlight))
+		return dev_err_probe(dev, PTR_ERR(ctx->panel.backlight),
+				     "Failed to create backlight\n");
 
 	drm_panel_add(&ctx->panel);
 
